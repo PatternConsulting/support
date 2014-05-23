b422ae2b227c4eae18318dc1d91e848be41b78e4
Michael Ahlers <michael@ahlers.co>
2014-05-23 14:04:30 -0400

    Don't duplicate the validation logic. Leave that up to the UUID class.

--- src/main/scala/nu/pattern/support/formats/UUIDFormat.scala
+++ src/main/scala/nu/pattern/support/formats/UUIDFormat.scala
@@ -4,17 +4,14 @@ import play.api.libs.json._
 import java.util.UUID
 import play.api.libs.json.JsString
 import play.api.libs.json.JsSuccess
+import scala.util.{Failure, Success, Try}
 
 object UUIDFormat extends Format[UUID] {
-  val uuidP = "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}".r
-
   override def reads(v: JsValue): JsResult[UUID] = v match {
     case JsString(s) =>
-      s.trim.toLowerCase match {
-        case uuidP() =>
-          JsSuccess(UUID.fromString(s.trim))
-        case _ =>
-          JsError( s"""Incorrect UUID format for \"$s\".""")
+      Try(UUID.fromString(s.trim)) match {
+        case Success(u) => JsSuccess(u)
+        case Failure(e) => JsError( s"""Invalid UUID \"$s\". (${e.getMessage})""")
       }
     case _ =>
       JsError( s"""Expected string value for UUID (got "$v").""")
--- src/test/scala/nu/pattern/support/formats/UUIDFormatSpec.scala
+++ src/test/scala/nu/pattern/support/formats/UUIDFormatSpec.scala
@@ -2,35 +2,46 @@ package nu.pattern.support.formats
 
 import org.specs2.mutable._
 import java.util.UUID
-import play.api.libs.json.{JsSuccess, JsString}
+import play.api.libs.json.{JsError, JsSuccess, JsString}
 
 class UUIDFormatSpec extends Specification {
-  val sample = UUID.randomUUID()
+  val valid = UUID.randomUUID()
 
-  s"""UUID "$sample"""" should {
+  s""""$valid"""" should {
     {
-      val expected = JsString(sample.toString)
+      val expected = JsString(valid.toString)
 
       s"""write as "$expected"."""" in {
-        UUIDFormat.writes(sample) must equalTo(expected)
+        UUIDFormat.writes(valid) must beEqualTo(expected)
       }
     }
 
     {
-      val expected = JsSuccess(sample)
+      val expected = JsSuccess(valid)
 
       s"""read as "$expected"."""" in {
-        UUIDFormat.reads(JsString(sample.toString)) must equalTo(expected)
+        UUIDFormat.reads(JsString(valid.toString)) must beEqualTo(expected)
       }
 
       s"""read as "$expected" (with mangled letter case)."""" in {
-        UUIDFormat.reads(JsString(sample.toString.toUpperCase)) must equalTo(expected)
+        UUIDFormat.reads(JsString(valid.toString.toUpperCase)) must beEqualTo(expected)
       }
 
       s"""read as "$expected" (with padding whitespace)."""" in {
-        UUIDFormat.reads(JsString("  " + sample.toString + "  ")) must equalTo(expected)
+        UUIDFormat.reads(JsString("  " + valid.toString + "  ")) must beEqualTo(expected)
       }
     }
+  }
+
+  s"""Reader""" should {
+    s"""return errors for junk values""" in {
+      UUIDFormat.reads(JsString("junk value")) must beAnInstanceOf[JsError]
+    }
+
+    s"""return errors when a UUID is incorrectly formatted""" in {
+      UUIDFormat.reads(JsString(valid.toString.replace('-', '_'))) must beAnInstanceOf[JsError]
+    }
+
   }
 
 }
