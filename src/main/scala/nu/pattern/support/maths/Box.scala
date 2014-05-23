bf1da8afe3a13447043fef10b9d40ccb614cbd04
Michael Ahlers <michael@ahlers.co>
2014-05-23 17:18:38 -0400

    Expand the Box type.

--- src/main/scala/nu/pattern/support/maths/Box.scala
+++ src/main/scala/nu/pattern/support/maths/Box.scala
@@ -1,7 +1,18 @@
 package nu.pattern.support.maths
 
-case class Box[T](x0: T, y0: T, x1: T, y1: T)(implicit ev: Numeric[T]) {
-  val width: T = ev.minus(x1, x0)
+import nu.pattern.support.maths.Box.Point
 
-  val height: T = ev.minus(y1, y0)
+case class Box[T](p0: Point[T], p1: Point[T])(implicit ev: Numeric[T]) {
+  val width: T = ev.minus(p1.x, p0.x)
+
+  val height: T = ev.minus(p1.y, p0.y)
+}
+
+object Box {
+
+  case class Point[T](x: T, y: T)(implicit ev: Numeric[T])
+
+  def apply[T](x: T, y: T, width: T, height: T)(implicit ev: Numeric[T]): Box[T] =
+    Box(Point(x, y), Point(ev.plus(x, width), ev.plus(y, height)))
+
 }
