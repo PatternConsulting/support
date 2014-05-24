b2ce6503422280ac50f0a7301ada2f6481b7b643
Michael Ahlers <michael@ahlers.co>
2014-05-23 21:09:12 -0400

    Force explicit selection of the Float to Int strategy.

---
+++ src/main/scala/nu/pattern/support/maths/NumberLIke.scala
@@ -0,0 +1,24 @@
+package nu.pattern.support.maths
+
+import scala.annotation.implicitNotFound
+
+/**
+ * [[Numeric]] doesn't support translating to parameterized types. This class offers implicit mixins for performing numeric type conversions.
+ */
+@implicitNotFound("No member of type class NumberLike in scope for ${T}.")
+trait NumberLike[S, T] {
+  /**
+   * Get a [[T]] for an [[S]]. There is no conversion back, however, as some numerical conversions result in a loss of precision or accuracy. Those cases require callers to explicitly act according to their needs.
+   */
+  def from(v: S): T
+}
+
+object NumberLike {
+
+  class IntFloatNumberLike(chop: Float => Int) extends NumberLike[Float, Int] {
+    override def from(v: Float): Int = chop(v)
+  }
+
+  object IntRoundFloatNumberLike extends IntFloatNumberLike(Math.round)
+
+}
