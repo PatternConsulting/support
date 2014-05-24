33de2c8f94996d0eda23bffd65150536a8f637f9
Michael Ahlers <michael@ahlers.co>
2014-05-23 20:48:42 -0400

    Simple 2D box.

--- src/main/scala/nu/pattern/support/maths/Box.scala
+++ src/main/scala/nu/pattern/support/maths/Box.scala
@@ -1,15 +1,7 @@
 package nu.pattern.support.maths
 
-import nu.pattern.support.maths.Box.Point
-
 case class Box[T](p0: Point[T], p1: Point[T])(implicit ev: Numeric[T]) {
   val width: T = ev.minus(p1.x, p0.x)
 
   val height: T = ev.minus(p1.y, p0.y)
-}
-
-object Box {
-
-  case class Point[T](x: T, y: T)(implicit ev: Numeric[T])
-
 }
