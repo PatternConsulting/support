308d724f7566703cdc82a462151f6dc5dd1fe274
Michael Ahlers <michael@ahlers.co>
2014-05-23 21:41:51 -0400

    Remove misguided method.

--- src/main/scala/nu/pattern/support/maths/PointLike.scala
+++ src/main/scala/nu/pattern/support/maths/PointLike.scala
@@ -4,5 +4,4 @@ import scala.annotation.implicitNotFound
 
 @implicitNotFound("No member of type class PointLike in scope for ${T}.")
 trait PointLike[T] {
-  def toFloat(p: Point[T])(implicit ev: Numeric[T]) = Point(ev.toFloat(p.x), ev.toFloat(p.y))
 }
