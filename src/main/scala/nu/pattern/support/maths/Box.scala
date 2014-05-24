d354874c382d2a3d097099928deee90f5aed7672
Michael Ahlers <michael@ahlers.co>
2014-05-23 21:49:38 -0400

    Implicit BoxLike.

--- src/main/scala/nu/pattern/support/maths/Box.scala
+++ src/main/scala/nu/pattern/support/maths/Box.scala
@@ -1,7 +1,16 @@
 package nu.pattern.support.maths
 
-case class Box[T](p0: Point[T], p1: Point[T])(implicit ev: Numeric[T]) {
+case class Box[T](p0: Point[T], p1: Point[T])(implicit ev: Numeric[T]) extends BoxLike[Box[T], T] {
   val width: T = ev.minus(p1.x, p0.x)
 
   val height: T = ev.minus(p1.y, p0.y)
+
+  override def from(v: Box[T]): Box[T] = v
+
+  override def to(v: Box[T]): Box[T] = v
+}
+
+object Box {
+  def apply[T](x0: T, y0: T, x1: T, y1: T)(implicit ev: Numeric[T]): Box[T] =
+    Box(Point(x0, y0), Point(x1, y1))
 }
