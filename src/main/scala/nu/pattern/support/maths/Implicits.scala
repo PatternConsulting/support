a35c9a2eb8a88d8305ddfdf5bfd4afb231662221
Michael Ahlers <michael@ahlers.co>
2014-05-23 21:42:41 -0400

    Fix evidences.

--- src/main/scala/nu/pattern/support/maths/Implicits.scala
+++ src/main/scala/nu/pattern/support/maths/Implicits.scala
@@ -3,7 +3,7 @@ package nu.pattern.support.maths
 object Implicits {
 
   implicit class BoxesOps[T, M](b: Seq[T])(implicit ev: BoxLike[T, M]) {
-    def intersections(implicit nlfev: NumberLike[Float, M]) = Boxes.intersections(b)
+    def intersections(implicit nev: Numeric[M], nlev: NumberLike[Float, M]) = Boxes.intersections(b)
   }
 
 }
