f39fcc6db137c68d0ba08d6ee49197ceb2f35a62
Michael Ahlers <michael@ahlers.co>
2014-05-26 19:26:39 -0400

    Make an object.

--- src/main/scala/nu/pattern/support/graphics/Implicits.scala
+++ src/main/scala/nu/pattern/support/graphics/Implicits.scala
@@ -2,7 +2,7 @@ package nu.pattern.support.graphics
 
 import java.awt.image.BufferedImage
 
-class Implicits {
+object Implicits {
 
   implicit class BufferedImageOps(i: BufferedImage) {
     def convertTo(`type`: Int) = BufferedImages.convertTo(i, `type`)
