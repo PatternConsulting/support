c91c9483f42842398eed2f364b7a49fa58fd2ebd
Michael Ahlers <michael@ahlers.co>
2014-05-26 17:00:50 -0400

    Fix a subtle flaw in the algorithm.

--- src/main/scala/nu/pattern/support/maths/Boxes.scala
+++ src/main/scala/nu/pattern/support/maths/Boxes.scala
@@ -12,39 +12,40 @@ object Boxes {
   def intersections[T](boxes: Seq[T])(implicit ev: archery.BoxLike[T]): Set[T] = {
     import archery.{Entry, Box, RTree}
 
-    val _boxes = boxes.map(ev.from)
-
     val entries =
-      _boxes
+      boxes
+        .map(ev.from)
         .zipWithIndex
         .map { case (v, i) => Entry(v, i)}
 
     val space = RTree(entries: _*)
 
-    def mkIntersection(b0: Box, b1: Box) = {
-      /* Find the right- and down-most top-left point. */
-      val x0 = Math.max(b0.x, b1.x)
-      val y0 = Math.max(b0.y, b1.y)
+    val intersectionBoxes = entries.map {
+      entry =>
+        val b0 = entry.geom.toBox
 
-      /* Find the left- and up-most bottom-right point. */
-      val x1 = Math.min(b0.x2, b1.x2)
-      val y1 = Math.min(b0.y2, b1.y2)
-
-      Box(x0, y0, x1, y1)
-    }
-
-    val intersectionBoxes0 = _boxes.map {
-      searchBox =>
-        /* Find all boxes intersecting the current search box, but exclude results which contain only the search box (that implicit suggests no overlap). */
+        /* Find all boxes that intersect the entry, but exclude the entry itself (as it'll participate in the intersection calcuation for each match). If there are no matches besides, then zero intersections will be produced. */
         val matchingBoxes =
-          Some(space.searchIntersection(searchBox).map(_.geom.toBox))
-            .filter(1 < _.size).getOrElse(Nil)
+          space
+            .searchIntersection(entry.geom.toBox)
+            .filterNot(_.value == entry.value)
+            .map(_.geom.toBox)
 
         /* Create the intersection between each matching box and the search box. Note this causes redundant results, but those will be eliminated when converting to a set and this is an optimization that can be made later. */
-        matchingBoxes.map(mkIntersection(searchBox, _))
+        matchingBoxes.map { b1 =>
+          /* Find the right- and down-most top-left point. */
+          val x0 = Math.max(b0.x, b1.x)
+          val y0 = Math.max(b0.y, b1.y)
+
+          /* Find the left- and up-most bottom-right point. */
+          val x1 = Math.min(b0.x2, b1.x2)
+          val y1 = Math.min(b0.y2, b1.y2)
+
+          Box(x0, y0, x1, y1)
+        }
     }.flatten
 
-    intersectionBoxes0.toSet.map(ev.to)
+    intersectionBoxes.toSet.map(ev.to)
   }
 
 }
