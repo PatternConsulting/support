40c3ca0cf7f0376b43d56746de2d02485bf7c166
Michael Ahlers <michael@ahlers.co>
2014-05-26 14:04:33 -0400

    Start crafting tests for Boxes.

--- src/test/scala/nu/pattern/support/maths/BoxesSpec.scala
+++ src/test/scala/nu/pattern/support/maths/BoxesSpec.scala
@@ -1,24 +1,19 @@
 package nu.pattern.support.maths
 
-import java.awt.Rectangle
-
-import org.specs2.mutable._
-
 import Implicits.BoxesOps
 import archery.Box
+import org.specs2.mutable._
 
 class BoxesSpec extends Specification {
 
-  val r0 = new Rectangle(10, 10, 100, 100)
-  val r1 = new Rectangle(20, 20, 200, 200)
-  val s0 = Seq(r0, r1)
+  "Intersections" should {
+    "not exist for disjoint boxes" in {
+      val boxes: Seq[Box] = for (i <- 0 to 9) yield {
+        Box(i * 10, i * 10, i * 10 + 5, i * 10 + 5)
+      }
 
-  s0.intersections
-
-  val b0 = Box(10, 10, 110, 110)
-  val b1 = Box(20, 20, 220, 220)
-  val s1 = Seq(b0, b1)
-
-  s1.intersections
+      boxes.intersections must beEmpty
+    }
+  }
 
 }
