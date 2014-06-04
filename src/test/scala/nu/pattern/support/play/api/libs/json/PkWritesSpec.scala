4e3d77df2e51ab81ff047c9b6b3e99baa7625369
Michael Ahlers <michael@ahlers.co>
2014-06-03 20:44:33 -0400

    Fix compilation error with Scala 2.11.

--- src/test/scala/nu/pattern/support/play/api/libs/json/PkWritesSpec.scala
+++ src/test/scala/nu/pattern/support/play/api/libs/json/PkWritesSpec.scala
@@ -1,26 +1,32 @@
 package nu.pattern.support.play.api.libs.json
 
-import anorm.{Id, NotAssigned, Pk}
+import anorm.{NotAssigned, Pk}
 import org.specs2.mutable._
 import play.api.libs.json._
-import play.api.libs.json.Json._
 import nu.pattern.support.play.api.libs.json.Implicits._
 import anorm.Id
 import play.api.libs.json.JsNumber
 
 class PkWritesSpec extends Specification {
 
-  def test[I](input: I, expected: JsValue)(implicit w: Writes[I]) {
+  def test[I](input: I, expected: JsValue)(implicit w: Writes[Pk[I]]) {
     s"""An ${input.getClass.getName} with value "$input"""" should {
       s"write to $expected" in {
-        val actual = toJson(input)
+        val actual = w.writes(Id(input))
+        actual must beEqualTo(expected)
+      }
+
+      s"write to $JsNull when $NotAssigned" in {
+        val na: Pk[I] = NotAssigned
+        val expected = JsNull
+        val actual: JsValue = w.writes(na)
         actual must beEqualTo(expected)
       }
     }
   }
 
-  test(Id("10"), JsString("10"))
-  test(Id(10L), JsNumber(10L))
-  test(NotAssigned, JsNull)
+  test("10", JsString("10"))
+  test(10L, JsNumber(10L))
+  test(10.01, JsNumber(10.01))
 
 }
