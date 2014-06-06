7bdd95cac413a8cebd2d4c98e8fa95e3cc2e5220
Michael Ahlers <michael@ahlers.co>
2014-06-05 21:14:56 -0400

    Fix the broken spec. by including the ClassTag for the id's value type.

--- src/main/scala/nu/pattern/support/anorm/Column.scala
+++ src/main/scala/nu/pattern/support/anorm/Column.scala
@@ -2,15 +2,16 @@ package nu.pattern.support.anorm
 
 import anorm.{Id, Pk, MetaDataItem, TypeDoesNotMatch}
 import anorm.Column._
+import scala.reflect.ClassTag
 
 /**
  * [[anorm.Column]] companion, providing default conversions.
  */
 object Column {
-  implicit def columnToPk[T]: anorm.Column[Pk[T]] = nonNull { (value, meta) =>
+  implicit def columnToPk[T: ClassTag]: anorm.Column[Pk[T]] = nonNull { (value, meta) =>
     val MetaDataItem(qualified, nullable, clazz) = meta
-    value match {
-      case v: T => Right(Id(v))
+    Id(value) match {
+      case v: Pk[T] => Right(v)
       case _ => Left(TypeDoesNotMatch(s"Cannot convert $value: ${value.asInstanceOf[AnyRef].getClass} to Pk for column $qualified"))
     }
   }
